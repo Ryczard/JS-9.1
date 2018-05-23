c728cfa3d0b6d8da396e8443bd02074514d900a8
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-23 12:50:28 +0200

    Update scripts.js content

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function getTriangleArea(a, h) {
 	if (a <= 0 || h <= 0){
 		console.log('Nieprawidłowe dane');
 	} else {
-		return a * h / 2
+		return a * h / 2;
 	}
 }
-console.log(getTriangleArea(10, 6))
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(-10, 15);
